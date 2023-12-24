9054385a44e45022c768d3d47654bfa8a3022ac9
jakubmat2001 <jakubmatusik11@gmail.com>
2023-12-24 18:00:20 +0000

    once again changed cors orgin access route

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(cors({
   origin: 
   ['http://localhost:3000', 
-  'https://portfolio.d3ssupx6aiji5o.amplifyapp.com'],
+  'https://portfolio.d30eostezad756.amplifyapp.com'],
 }));
 
 app.get("/", (req, res) => {
